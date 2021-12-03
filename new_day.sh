2e81f9f9a8dda8720be928f7b45d47f73c7b9f15
Chase Carnaroli <chasecarnaroli@gmail.com>
2021-12-02 19:03:32 -0800

    Update new_day script

--- new_day.sh
+++ new_day.sh
@@ -1,4 +1,4 @@
 mkdir $1;
 touch $1/input.txt;
 touch $1/part1.py
-echo "file_name = 'input.txt'\n\nfor line in open(file_name):\n" >> $1/part1.py
+printf "file_name = 'input.txt'\n\nfor line in open(file_name):\n" >> $1/part1.py
