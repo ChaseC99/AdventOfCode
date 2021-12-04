1c21d9a8a0fc1da9670b65ad7a89b1246259ee6d
Chase Carnaroli <chasecarnaroli@gmail.com>
2021-12-03 17:42:46 -0800

    Improve newday script, add session cookie to .env file

--- new_day.sh
+++ new_day.sh
@@ -1,4 +1,30 @@
-mkdir $1;
-touch $1/input.txt;
-touch $1/part1.py
-printf "file_name = 'input.txt'\n\nfor line in open(file_name):\n" >> $1/part1.py
+#!/bin/bash
+
+# Load the session cookie from the .env file
+source ./.env
+echo $SESSION
+
+# Name the day paramater
+day=$1
+
+# Create folder for the day
+mkdir $day
+
+# Create the files with some boilerplate code
+touch $day/part1.py
+printf "file_name = 'input.txt'\n\nfor line in open(file_name):\n" >> $day/part1.py
+touch $day/part2.py
+
+# Create demo and input files. 
+touch $day/demo.txt
+touch $day/input.txt
+
+# Format the day variable
+# For single digit numbers, we need to remove the leading 0
+# E.g. 01 -> 1
+[[ ${day:0:1} = '0' ]] && day="${day:1}"
+
+# Fetch the day's input from adventofcode.com
+curl -b "session=$SESSION" https://adventofcode.com/2021/day/$day/input > $1/input.txt
+# Remove the newline at the end of the input file
+echo -n "$(cat $1/input.txt)" >| "$1/input.txt"
