f5b13a3b72bb29ccb21e43746e48c662118df29b
Chase Carnaroli <chase@khanacademy.org>
2023-12-05 22:55:38 -0800

    Day 5 (part 1)

---
+++ 05/part2.py
@@ -0,0 +1,65 @@
+file_name = 'input.txt'
+
+class Map:
+    def __init__(self, name):
+        self.name = name
+        self.mappers = []
+    
+    def add_map(self, map_str):
+        destination, source, range_len = [int(value) for value in map_str.split(' ')]
+        def mapper(value):
+            if value in range(source, source + range_len):
+                return destination + (value - source)
+            else:
+                return value
+        self.mappers.append(mapper)
+
+    def map(self, value):
+        for mapper in self.mappers:
+            mapped_value = mapper(value)
+            if mapped_value != value:
+                return mapped_value
+        
+        return value
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+
+
+# def create_map(destination, source, range):
+#     def mapper(value):
+
+
+lines = []
+for line in open(file_name):
+    lines.append(line.strip())
+
+seeds = [int(seed) for seed in lines[0].split(':')[1].strip().split(' ')]
+seed_pairs = [(seeds[i], seeds[i+1]) for i in range(0, len(seeds), 2)]
+print(seed_pairs)
+
+i = 2
+maps = []
+while i < len(lines):
+    line = lines[i]
+
+    if line == '':
+        pass
+    elif line[0].isdigit():
+        maps[-1].add_map(line)
+    else:
+        maps.append(Map(line.split(" ")[0]))
+    
+    i += 1
+
+min_seed = seeds[0]
+for seed_pair in seed_pairs:
+    for seed in range(seed_pair[0], seed_pair[0]+seed_pair[1]):
+        for map in maps:
+            seed = map.map(seed)
+        min_seed = min(min_seed, seed)
+
+print(min_seed)
